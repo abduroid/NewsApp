7357d2641c233519c6b50e42a8b999b3c0f92368
abdulaziz_thinkland <abdulaziz@thinkland.uz>
2020-08-21 13:21:11 +0500

    Initial commit

---
+++ app/src/main/java/com/abduqodirov/newsapp/MyLoader.java
@@ -0,0 +1,33 @@
+package com.abduqodirov.newsapp;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import com.abduqodirov.newsapp.model.News;
+
+import java.util.List;
+
+public class MyLoader extends AsyncTaskLoader<List<News>> {
+
+    private String url;
+
+    public MyLoader(Context context, String url) {
+        super(context);
+        this.url = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<News> loadInBackground() {
+        if (url == null || url.isEmpty()) {
+            return null;
+        }
+
+        List<News> fetchedNews = QueryUtils.fetchNews(url);
+        return fetchedNews;
+    }
+}
